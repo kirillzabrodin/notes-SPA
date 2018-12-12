579f5afb3f1af948daa5029a0667a7af56fd4d5c
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-12 10:40:54 +0000

    working styling

--- test/Ruzilian.js
+++ test/Ruzilian.js
@@ -1,17 +1,16 @@
-function describe(def, func) {
-    document.getElementById('results').innerHTML += '<h3>' + def + '</h3>';
+function describe(def, func, header = "h1") {
+    document.getElementById('results').innerHTML += '<' + header + '>' + def + '</' + header + '>';
     return func();
 }
 
-const it = (def, func) => describe("\xa0\xa0" + def, func);
+const it = (def, func) => describe("\xa0\xa0" + def, func, 'h2');
 
 const matchers = (exp) => ({
   toEqual: (value) =>  {
     if (exp === value) {
       document.getElementById('results').innerHTML += "<h3 style='color: green;'>\xa0\xa0\xa0\xa0Pass</h3><br>";
     } else {
-      document.getElementById('results').innerHTML += "<h3 style='color: red;'>\xa0\xa0\xa0\xa0Fails<br>Expected:" + exp + " but got " + value + "</h3><br>";
-      // document.getElementById('results').innerHTML += console.trace(exp);
+      document.getElementById('results').innerHTML += "<h3 style='color: red;'>\xa0\xa0\xa0\xa0Fails Expected:" + exp + " but got " + value + "</h3><br>";
     }
   }
 });
