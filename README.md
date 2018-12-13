4093a0631f64f6d09357293218fcba3c140c5e76
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-13 03:28:59 +0000

    added more README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # A Single Page Notes App
 This is a single page notes app built solely in JavaScript. You can add notes without refreshing the page.
+
+## Installation
+For local viewing only use
+```
+git clone git@github.com:kirillzabrodin/notes-SPA.git
+open ./Relative_path/to_the/Project_folder/index.html
+```
+for localhost hosting
+```
+git clone git@github.com:kirillzabrodin/notes-SPA.git
+npm install
+npm start
+open http://localhost:8080
+```
+Please note that 8080 is a default and might be different for you.
+
+## Testing
+
+Testing is done via a framework created by yours truly and Luisa Theodoro. It is called Ruzilian and to run it all you have to do is look at the SteroidNeymat.html page in a browser.
+
+## Contributing
+
+Please feel free to submit a Pull Request with additional code if you think some further functionality would be useful. The criteria are for the code to be solely vanilla JS and it must be tested using the framework(that could be expanded upon via a pull request too).
+
+## Deployment
+
+Type
+```
+heroku create
+```
+into the terminal and wait for the command to execute. Check that ```git remote -v``` shows the new heroku remote and then deploy by pushing to the master branch of the heroku remote.
+
+## Live Demo
+
+The app is currently deployed on https://stormy-escarpment-10799.herokuapp.com/#home and can be accessed there. Any questions feel free to open issues.
--- test/views-test.js
+++ test/views-test.js
@@ -1,22 +1,22 @@
 describe('Views tests', function() {
 
-  describe('#RenderNotes', function() {
-    it('returns note title in a p tag', function() {
+  describe('#notesPage', function() {
+    it('returns note page with notes', function() {
       var notepad = new NotesList();
       notepad.createNote("Hello");
-      var render = new Render(notepad);
-      expected = "<a id='6' href='#6'>Hello</a><br>"
-      expect(render.RenderNotes(notepad)).toEqual(expected);
+      var render = new Render();
+      expected = "<h3 class='btn'>Hello Ed, I am not a killer machine and I'm definitely not tracking you :)</h3><br><textarea rows='4' cols='50' id='note-input' placeholder='Enter text here:'></textarea><br><a id='submit' class='btn'>Submit Note</a><div id='notes'></div><br><div id='full-note'></div><a class='btn' id='6' href='#6'>Hello</a><br>"
+      expect(render.notesPage(notepad)).toEqual(expected);
     })
   })
 
-  describe('#RenderNoteText', function() {
+  describe('#noteFull', function() {
     it('returns note text in a p tag', function() {
       var notepad = new NotesList();
       notepad.createNote("Hello");
-      var render = new Render(notepad);
-      expected = "<a class='full' href='#home' id='7full'>Hello</a>"
-      expect(render.RenderNoteText(7,notepad)).toEqual(expected);
+      var render = new Render();
+      expected = "<div id='notes'></div><br><div id='full-note'></div><a class='btn' id='7full'>Hello</a>"
+      expect(render.noteFull(notepad,7)).toEqual(expected);
     })
   })
 
